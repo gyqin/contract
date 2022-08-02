46896f839230cb5f73236eef0ad15900818f6901
qingy <guangyuegg@163.com>
2022-08-02 17:07:17 +0800

    streamTest

---
+++ contract/src/main/java/com/example/stream/testStream.java
@@ -0,0 +1,10 @@
+package com.example.stream;
+
+import java.util.stream.Stream;
+
+public class testStream {
+    public static void main(String[] args) {
+        Stream.of("a", "b");
+    }
+
+}
--- contract/src/test/java/com/example/contract/ContractApplicationTests.java
+++ contract/src/test/java/com/example/contract/ContractApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.contract;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.stream.Stream;
+
 @SpringBootTest
 class ContractApplicationTests {
 
   @Test
   void contextLoads() {
+    Stream<String> a = Stream.of("a", "b", "c");
   }
 
 }
